0fd3a517bc577e25fb79f1d11cbed748e4b7899f
meghanajj <meghanajadav4@gmail.com>
2023-10-07 15:26:49 +0530

    code review fix

--- Selenium_Assignments/Week4/src/main/java/selenium/Week4/Question1.java
+++ Selenium_Assignments/Week4/src/main/java/selenium/Week4/Question1.java
@@ -1,6 +1,6 @@
 package selenium.Week4;
 
-import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,30 +10,41 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Question1 {
 
-	    public static void main(String[] args) throws InterruptedException {
+	    public static void main(String[] args){
 	    	
-		
-	    	ChromeOptions co = new ChromeOptions();
+	    	//use chrome options to resolve web socket issue
+			ChromeOptions co = new ChromeOptions();
 			co.addArguments("--remote-allow-origins=*");
-			
-	    	//create an instance of web driver interface
-	        WebDriver driver;
-            //creating obj of chrome driver class
-	        driver = new ChromeDriver(co);
-	        driver.manage().window().maximize();
+
+			// create an instance of web driver interface
+			WebDriver driver;
+			// creating obj of chrome driver class
+			driver = new ChromeDriver(co);
+			//maximise the window
+			driver.manage().window().maximize();
+			//get the url 
 	        driver.get("https://jqueryui.com/datepicker/#buttonbar");
 	        
 	        //search for frame and switch to frame
 	        WebElement Frame = driver.findElement(By.className("demo-frame"));
 			driver.switchTo().frame(Frame);
 			
-	        //select the date picker
-	        WebElement datePicker = driver.findElement(By.className("hasDatepicker"));
-	        datePicker.click();
-	        //select the date and click
-	        WebElement date = driver.findElement(By.xpath("//td/a[text()='21']"));
-	        date.click();
-
+			try {
+						
+			        //select the date picker
+			        WebElement datePicker = driver.findElement(By.className("hasDatepicker"));
+			        datePicker.click();
+			        //select the date and click
+			        WebElement date = driver.findElement(By.xpath("//td/a[text()='21']"));
+			        date.click();
+			}
+			
+			catch (NoSuchElementException e) {
+				
+				System.out.println(e);
+			}
+			
+			//close the driver at the end 
 	        driver.quit();
 			
 	    }
